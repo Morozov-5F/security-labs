d356f24b7e8fdd07828ba031d444193fd603ce9b
Eugene Morozov <zhe3145@yandex.ru>
2017-11-14 23:07:57 +0300

    [stegano-detector] Classificator preparations

--- stegano-detector/main.cpp
+++ stegano-detector/main.cpp
@@ -4,13 +4,12 @@
 #include <fstream>
 
 #include <map>
-
-#include <iomanip>
-#include <jpeglib.h>
+#include <vector>
 
 extern "C"
 {
 #include <jpeglib.h>
+#include <getopt.h>
 }
 
 typedef struct error_mgr_s
@@ -65,21 +64,51 @@ s_read_coefficients(jpeg_decompress_struct * decompress_info, jvirt_barray_ptr *
         {
             for (JDIMENSION block_num = 0; block_num < width_in_blocks; ++block_num)
             {
-                std::cout << std::endl << "** DCT block, component " << component_num << " **" << std::endl;
                 for (JDIMENSION i = 0; i < DCTSIZE2; ++i)
                 {
                     short dc = row_pointers[component_num][row_num][block_num][i];
-
-                    std::cout << std::setfill(' ') << std::setw(6) << dc;
-                    if(i % 8 == 7)
-                    {
-                        std::cout << std::endl;
-                    }
                 }
             }
         }
     }
     return row_pointers;
+}
+
+static void
+s_calculate_distribution_for_rows(jpeg_component_info * component_info, JBLOCKARRAY component)
+{
+    std::map<short, unsigned> coefficient_map_row;
+
+    assert(nullptr != component);
+    assert(nullptr != component_info);
+
+    std::cout << "Calculating coefficients distribution for each row... ";
+    size_t total_coeffs = 0;
+    std::vector<double> distributions_row;
+
+    for (auto i = 0; i < component_info->height_in_blocks; ++i)
+    {
+        for (auto j = 0; j < component_info->width_in_blocks; ++j)
+        {
+            for (auto k = 1; k < DCTSIZE2; ++k)
+            {
+                if (component[i][j][k] == 1 || component[i][j][k] == 0)
+                {
+                    continue;
+                }
+                auto coeff = component[i][j][k] & 0b00000011;
+                coefficient_map_row[coeff] += 1;
+                ++total_coeffs;
+            }
+        }
+        std::cout << "Row number " << i << std::endl;
+        for (auto iter = coefficient_map_row.begin(); iter != coefficient_map_row.end(); ++iter)
+        {
+            std::cout << iter->first << " " << iter->second << std::endl;
+        }
+        coefficient_map_row.clear();
+        total_coeffs = 0;
+    }
 }
 
 static void
@@ -91,12 +120,12 @@ s_make_coeff_histogram(jpeg_component_info * component_info, JBLOCKARRAY compone
     assert(nullptr != component_info);
 
     std::cout << "Creating JPEG coefficients histogram... ";
+    size_t total_coeffs = 0;
 
     for (auto i = 0; i < component_info->height_in_blocks; ++i)
     {
         for (auto j = 0; j < component_info->width_in_blocks; ++j)
         {
-            short prev_byte = component[i][j][0] & 0b00000011;
             for (auto k = 1; k < DCTSIZE2; ++k)
             {
                 if (component[i][j][k] == 1 || component[i][j][k] == 0)
@@ -105,6 +134,7 @@ s_make_coeff_histogram(jpeg_component_info * component_info, JBLOCKARRAY compone
                 }
                 auto coeff = component[i][j][k] & 0b00000011;
                 coefficient_map[coeff] += 1;
+                ++total_coeffs;
             }
         }
     }
@@ -121,19 +151,31 @@ s_make_coeff_histogram(jpeg_component_info * component_info, JBLOCKARRAY compone
     return;
 }
 
+static int
+check_jpeg_coeffs_hist()
+{
+
+}
+
+static int
+check_jpeg_coeffs_lsb_transitions()
+{
+
+}
+
 int
-process_jpeg_file(const char * filename)
+process_jpeg_file(std::string filename)
 {
     JBLOCKARRAY * row_pointers = nullptr;
     struct jpeg_decompress_struct cinfo;
     error_mgr_t                   jerr;
 
-    jvirt_barray_ptr  * jpeg_coefficients = NULL;
+    jvirt_barray_ptr  * jpeg_coefficients = nullptr;
 
     FILE * in_file = nullptr;
 
-    in_file = fopen(filename, "rb");
-    if (NULL == in_file)
+    in_file = fopen(filename.c_str(), "rb");
+    if (nullptr == in_file)
     {
         std::cout << "Unable to open file " << filename << std::endl;
         return EXIT_FAILURE;
@@ -164,13 +206,74 @@ process_jpeg_file(const char * filename)
 
     auto component_index = 0;
     s_make_coeff_histogram(&cinfo.comp_info[component_index], row_pointers[component_index]);
-
+    s_calculate_distribution_for_rows(&cinfo.comp_info[component_index], row_pointers[component_index]);
     return EXIT_SUCCESS;
 }
 
-int main ()
+const char OPTIONS[] = "a:";
+
+int
+parse_options(int argc, char *const * argv, std::string & filename, int &teach_value)
 {
-    std::cout << "Hello, World!" << std::endl;
-    process_jpeg_file("result_image.jpg");
-    return 0;
+    int retval = EXIT_SUCCESS;
+    int teach_value_internal = 0;
+
+    int option;
+
+    option = getopt(argc, argv, OPTIONS);
+    while (option != -1)
+    {
+        std::string::size_type sz;
+        switch (option)
+        {
+            case 'a':
+                teach_value_internal = std::atoi(optarg);
+                if (teach_value_internal != -1 && teach_value_internal != 1)
+                {
+                    std::cerr << "Answer should be 1 for positive and -1 for negative teach result" << std::endl;
+                }
+                break;
+            default:
+                retval = EXIT_FAILURE;
+        };
+
+        option = (char)getopt(argc, argv, OPTIONS);
+    }
+
+    if (retval == EXIT_FAILURE)
+    {
+        return retval;
+    }
+
+    if (optind >= argc)
+    {
+        std::cerr << "Wrong number of arguments" << std::endl;
+    }
+
+    filename = argv[optind];
+    teach_value = teach_value_internal;
+    std::cout << "File to process: " << filename << std::endl;
+    std::cout << "Mode:            " << ((teach_value == 0) ? ("Detect") : ("Learn")) << std::endl;
+    if (teach_value != 0)
+    {
+        std::cout << "Answer:          " << ((teach_value == -1) ? ("Empty container") : ("Filled container")) << std::endl;
+    }
+
+    return retval;
+}
+
+int
+main(int argc, char *const * argv)
+{
+    std::string file_name;
+    int teach_value;
+
+    if (parse_options(argc, argv, file_name, teach_value) != 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    process_jpeg_file(file_name);
+
+    return EXIT_SUCCESS;
 }
